0974ef4730e987923e067e1579dd713a0e68ce42
Abeeaad97 <abidbakhtiyar@csu.fullerton.edu>
2019-04-14 18:23:17 -0700

    Added Comments

--- virt_mem.c
+++ virt_mem.c
@@ -1,8 +1,9 @@
 //Abid Bakhtiyar
 //890459241
+//Project 2: Virtual Memory
 //CPSC 351
-//McCarthy 
-//Project 2: Virtual Memory 
+//McCarthy
+
 #include <stdio.h>
 #include <sys/mman.h>
 #include <sys/types.h>
@@ -11,7 +12,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int positive(int a, int b)
+int positive(int a, int b) //Makes sure the Cache size is positive
 {
     if(a>b)
     return a;
@@ -19,8 +20,8 @@ int positive(int a, int b)
 }
 
 #define TBUFF_SIZE 16 //Size of the cache
-#define P_FRAMES 256 //nuber of page frames, change this to 128 if you want to translate out of 128 bits
-#define P_MASK 255 // change to 127
+#define P_FRAMES 128 //nuber of page frames, change this to 128 if you want to translate out of 128 bits
+#define P_MASK 127 // change to 127
 #define P_SIZE 256
 #define O_BITS 8 // number of bits for the offset
 #define O_MASK 255 
@@ -41,7 +42,7 @@ struct TLB tlb[TBUFF_SIZE]; //struct array for the cache is the size of the cach
 int pgTable[P_FRAMES]; //physical page number for the virtual address
 signed char memory [MEM_SIZE];
 
-int tlb_num = 0;
+int tlb_num = 0;//Index for running through the Cache Buffer
 
 //pointer for backing file
 signed char *back_store;
@@ -75,13 +76,13 @@ void fifo_add(unsigned char virt, unsigned char phys){
     entry -> phys_address = phys;
 }
 
+//Prints all stats
 void print(int a, int p, int t)
 {
-    
-    printf("Number of Addresses = %d\n", a);
-    printf("Page Faults = %d\n", p);
+    printf("------------------\n");
+    printf("    Statistics    \n");
+    printf("------------------\n");
     printf("Page Faults Rate = %.3f\n", p/(1. * a));
-    printf("TLB Hits = %d\n", t);
     printf("TLB Hit Rate = %.3f\n", t/(1. * a));
 }
 int main(int argc, const char*argv[])
@@ -91,12 +92,13 @@ int main(int argc, const char*argv[])
         fprintf(stderr, "Please run code with ./virt_mem addresses.txt");
         exit(ARGC_ERROR);
     }
-    char buff[BUFLEN];
-    int addCounter = 0;
-    int tlbHits = 0;
-    int pgFaults = 0;
+    char buff[BUFLEN]; //Buffer array
+    int addCounter = 0; //Counts the number of addresses
+    int tlbHits = 0; //Counts the number fo TLB Hits
+    int pgFaults = 0; //Counts the number of Page Faults
     unsigned char free_pg = 0; //next unallocated page in memory
 
+    //Opens Address File
     const char *filename = argv[1];
     FILE *fp = fopen(filename, "r");
     if(fp == NULL)
@@ -105,6 +107,7 @@ int main(int argc, const char*argv[])
         exit(FILE_ERROR);
     }
 
+    //Opens Backing Store File
     int backing = open("BACKING_STORE.bin", O_RDONLY);
     back_store = mmap(0, MEM_SIZE, PROT_READ, MAP_PRIVATE, backing, 0);
 
@@ -114,14 +117,15 @@ int main(int argc, const char*argv[])
         pgTable[i] = -1;
     }
 
+    //RUns through each address and gets the page and offset and translates it
     while(fgets(buff, BUFLEN, fp) != NULL)
     {
         addCounter++;
         int virtAdd = atoi(buff);
-        int offset = getOffset(virtAdd);
-        int virtPage = getPage(virtAdd);
+        int offset = getOffset(virtAdd);//Find the offset
+        int virtPage = getPage(virtAdd);//Finds the virtual page
 
-        int physPage = translate(virtPage);
+        int physPage = translate(virtPage); //Translates the Virtual Page into a Physical Page
 
         //Check for TLB hit or miss
 
@@ -147,16 +151,24 @@ int main(int argc, const char*argv[])
             fifo_add(virtPage, physPage);
         }
 
-        int physAdd = (physPage << O_BITS) | offset;
-        signed char value = memory[physPage * P_SIZE + offset];
+        int physAdd = (physPage << O_BITS) | offset; //Physical Address
+        signed char value = memory[physPage * P_SIZE + offset];//Value
         printf("Virtual address: %d Physical Address: %d Value: %d \n", virtAdd, physAdd, value);
         
 
 
     }
-    print(addCounter, pgFaults, tlbHits);
+    print(addCounter, pgFaults, tlbHits);//Function will Print stats
     return 0;
 }
+
+
+
+
+
+
+
+
 
 
 
